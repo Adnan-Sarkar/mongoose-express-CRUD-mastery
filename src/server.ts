7968fc9a109eb7f08ce83e780ee335458d504c1a
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-11-23 20:04:22 +0600

    connect mongoDB using mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
+import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config/config";
 
-app.listen(config.port, () => {
-  console.log(`SERVER is listening to the PORT: ${config.port}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`SERVER is listening to the PORT: ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
