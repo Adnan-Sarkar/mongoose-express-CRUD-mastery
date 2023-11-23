a06306725de096d3e1aebd76d7a20c68facb0de6
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-11-23 17:16:39 +0600

    created user model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,86 @@
+import { Schema, model } from "mongoose";
+import TUser, { TOrder, UserModel } from "./user.interface";
+
+const orderSchema = new Schema<TOrder>({
+  productName: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const userSchema = new Schema<TUser, UserModel>({
+  userId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+  },
+  address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  orders: {
+    type: [orderSchema],
+  },
+});
+
+// create user model
+const User = model<TUser, UserModel>("User", userSchema);
+
+// define static method functionality
+userSchema.statics.isUserExists = async (userId: string) => {
+  return await User.findOne({ userId });
+};
+
+export default User;
