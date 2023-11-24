613f9b2f71410ada69a8d36f2329c94f3d55763f
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-11-24 23:37:01 +0600

    Add base route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { UserRoutes } from "./app/modules/user/user.route";
 
@@ -8,5 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", UserRoutes);
+
+// welcome route
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to the Mongoose Express CRUD API",
+    details: "For API details, visit /api/users",
+    documentation:
+      "For detailed documentation, visit: https://github.com/Adnan-Sarkar/mongoose-express-CRUD-mastery",
+  });
+});
 
 export default app;
